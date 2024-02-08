02e39c47a87795d6828cd8c0e34ee1ff30921962
groth <groth@DESKTOP-LG49L58>
2024-02-08 19:31:03 +0100

    Projektdateien hinzufügen.

---
+++ test für ottonium/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ottonium_Spiel_Projektwoche
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            OttoniumSpiel spiel = new OttoniumSpiel();
+
+            Console.WriteLine("Wilkommen beim Ottonium spiel");
+            Console.WriteLine("Zum drücken einer LED, geben sie die Spalte und Zeile als Nummer an (1-5).");
+            Console.WriteLine("Das Ziel ist es, alle Lichter auszuschalten.");
+
+            while (!spiel.IstGelöst())
+            {
+                Console.WriteLine("\nAktuelles Spielfeld:");
+                spiel.SpielfeldAnzeigen();
+
+                Console.Write("\nBitte geben Sie die Zeilennummer (Horizontale) der LED an : ");
+                int zeile = int.Parse(Console.ReadLine());
+                Console.Write("Bitte geben Sie die Spaltennummer (Vertikale) der LED an ");
+                int spalte = int.Parse(Console.ReadLine());
+
+                spiel.KnopfDrücken(zeile, spalte);
+            }
+
+            Console.WriteLine("\nHerzlichen Glückwunsch! Sie haben das Spiel gelöst!");
+            Console.WriteLine("Eine Fanfare ertönt zur Feier Ihres Erfolgs!");
+
+            
+        }
+    }
+}
---
+++ test für ottonium/Spielfeld.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+class OttoniumSpiel
+{
+    private const int REIHEN = 5;
+    private const int SPALTEN = 5;
+    private int[,] spielfeld = new int[REIHEN, SPALTEN];
+
+    public OttoniumSpiel()
+    {
+        // gibt ein Spielfeld mit einem Zufälligem LED aus
+        Random rand = new Random();
+        for (int i = 0; i < REIHEN; i++)
+        {
+            for (int j = 0; j < SPALTEN; j++)
+            {
+                spielfeld[i, j] = rand.Next(2); // 0 für aus, 1 für an
+            }
+        }
+    }
+
+    public void KnopfDrücken(int zeile, int spalte)
+    {
+        // Umschalten des gedrückten Knopfes und seiner Nachbarn
+        Umschalten(zeile, spalte);
+        Umschalten(zeile - 1, spalte); // überprüfe oben
+        Umschalten(zeile + 1, spalte); // überprüfe unten
+        Umschalten(zeile, spalte - 1); // überprüfe links
+        Umschalten(zeile, spalte + 1); // überprüfe rechts
+    }
+
+    private void Umschalten(int zeile, int spalte)
+    {
+        if (zeile >= 0 && zeile < REIHEN && spalte >= 0 && spalte < SPALTEN)
+        {
+            spielfeld[zeile, spalte] = 1 - spielfeld[zeile, spalte]; // Schalte das Licht um (0 wird zu 1 und umgekehrt)
+        }
+    }
+
+    public bool IstGelöst()
+    {
+        // Überprüfe, ob alle Lichter ausgeschaltet sind
+        for (int i = 0; i < REIHEN; i++)
+        {
+            for (int j = 0; j < SPALTEN; j++)
+            {
+                if (spielfeld[i, j] == 1)
+                {
+                    return false; // Es gibt noch ein eingeschaltetes Licht
+                }
+            }
+        }
+        return true; // Alle Lichter sind ausgeschaltet
+    }
+
+    public void SpielfeldAnzeigen()
+    {
+        // Ausgabe des aktuellen Spielfelds
+        for (int i = 0; i < REIHEN; i++)
+        {
+            for (int j = 0; j < SPALTEN; j++)
+            {
+                if (spielfeld[i, j] == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red; // Setzt die Farbe auf Rot für die LEDS die an sind
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue; // Setzt die Farbe auf Weiß für die LEDS die aus sind
+                }
+                Console.Write("O "); // O für eingeschaltetes Licht
+            }
+            Console.WriteLine();
+        }
+        Console.ForegroundColor = ConsoleColor.White; // Setzt die Farbe wieder auf Weiß
+    }
+}
+
+
+
